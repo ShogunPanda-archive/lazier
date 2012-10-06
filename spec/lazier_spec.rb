d84e81d1841a8bf1e409d9560dce1359d4e57cb8
ShogunPanda <shogun_panda@me.com>
2012-10-06 08:16:01 +0200

    Added CodeClimate

--- spec/lazier_spec.rb
+++ spec/lazier_spec.rb
@@ -1,3 +1,9 @@
+# encoding: utf-8
+#
+# This file is part of the lazier gem. Copyright (C) 2012 and above Shogun <shogun_panda@me.com>.
+# Licensed under the MIT license, which can be found at http://www.opensource.org/licenses/mit-license.php.
+#
+
 require "spec_helper"
 
 describe Lazier do
