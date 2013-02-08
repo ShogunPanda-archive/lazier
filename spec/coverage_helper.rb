34b2ab4c5d60e1598e266ecdb3f3a8f879969098
ShogunPanda <shogun_panda@me.com>
2013-02-07 23:26:51 -0800

    Update Rakefile - Use of kramdown instead of redcarpet

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -5,16 +5,13 @@
 #
 
 require "pathname"
-
-if ENV["LAZIER_COVERAGE"] == "TRUE" && RUBY_VERSION >= "1.9" then
-  require "simplecov"
+require "simplecov"
   
+SimpleCov.start do
   root = Pathname.new(File.dirname(__FILE__)) + ".."
 
-  SimpleCov.start do
-    add_filter do |src_file|
-      path = Pathname.new(src_file.filename).relative_path_from(root).to_s
-      path !~ /^lib/
-    end
+  add_filter do |src_file|
+    path = Pathname.new(src_file.filename).relative_path_from(root).to_s
+    path !~ /^lib/
   end
 end
